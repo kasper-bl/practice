e8992116bc4aba61bbb4e653aad53956c90cd367
Kasper <polinalevcenko55@gmail.com>
2025-11-10 10:51:00 +0700

    add a skeleton

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.urls import include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('catalog/', include('catalog.urls')),
+    path( '', RedirectView.as_view(url='/catalog/', permanent=True)),
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
