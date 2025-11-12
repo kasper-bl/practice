7dab5531b14a6a9083d96a79c1f651bc95e31d4c
Kasper <polinalevcenko55@gmail.com>
2025-11-12 23:25:15 +0700

    add borrowed

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
 
+from django.contrib.auth.models import User
+
 class Genre(models.Model):
     """
     Model representing a book genre (e.g. Science Fiction, Non Fiction).
@@ -75,6 +77,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -91,6 +95,12 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return f"{self.id}, {self.book.title}"
+    
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 class Author(models.Model):
     """ Model representing an author."""
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre, Language
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 def index(request):
 
     num_books = Book.objects.all().count()
@@ -43,4 +45,14 @@ class AuthorDetailView(generic.DetailView):
 
 class AuthorUpdateView(generic.UpdateView):
     model = Author
+
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
