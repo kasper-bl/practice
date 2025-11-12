8a3e0659798a2d0903010d818d24b9d6e56cde42
Kasper <polinalevcenko55@gmail.com>
2025-11-12 09:11:06 +0700

    The admin panel has been completed

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,29 +1,35 @@
 from django.contrib import admin
 
-from .models import Author, Genre, Book, BookInstance
+from .models import Author, Genre, Book, BookInstance, Language
 
 
 #admin.site.register(Book)
 #admin.site.register(Author)
 admin.site.register(Genre)
+admin.site.register(Language)
 #admin.site.register(BookInstance)
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
 
+class BooksInline(admin.TabularInline):
+    model = Book
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-admin.site.register(Author, AuthorAdmin)
-
+    inlines = [BooksInline]
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id')
 
     fieldsets = (
         (None, {
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -47,9 +47,7 @@ class Book(models.Model):
     
 
 class BookInstance(models.Model):
-    """
-    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
-    """
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
@@ -69,9 +67,6 @@ class BookInstance(models.Model):
 
 
     def __str__(self):
-        """
-        String for representing the Model object
-        """
         return f"{self.id}, {self.book.title}"
 
 class Author(models.Model):
